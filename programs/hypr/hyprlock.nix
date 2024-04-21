a38ab4e88c7eccaf69c045cab1936a8041cd81f6
Autumn McKee <autumn@mck.is>
2024-04-21 11:50:31 +0100

    no need to keep using specific commit of hyprlock, just use release

--- programs/hypr/hyprlock.nix
+++ programs/hypr/hyprlock.nix
@@ -7,17 +7,4 @@
   environment.systemPackages = with pkgs; [
     hyprlock
   ];
-
-  nixpkgs.overlays = [
-    (final: prev: {
-      hyprlock = prev.hyprlock.overrideAttrs (old: {
-        src = prev.fetchFromGitHub {
-          owner = "hyprwm";
-          repo = "hyprlock";
-          rev = "2ae79757d5e5c48de2f4284992a6bfa265853a2d";
-          hash = "sha256-bhep83LkF7tGERJquZ8J54XTFj0GLLXXMJP0EY/5/7o=";
-        };
-      });
-    })
-  ];
 }
