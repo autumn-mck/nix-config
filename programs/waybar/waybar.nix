36ee00671e05e65f440eca4f51dba1c8ce9db95c
Autumn McKee <autumn@mck.is>
2024-07-05 13:49:20 +0100

    include waybar config

---
+++ programs/waybar/waybar.nix
@@ -0,0 +1,17 @@
+{ home-manager, pkgs, ... }:
+
+{
+  options = {
+    waybar.enable = lib.mkEnableOption "waybar";
+  };
+
+  config = lib.mkIf (config.waybar.enable) {
+    home.packages = with pkgs; [
+      waybar
+    ];
+
+    xdg.configFile."waybar/config.jsonc".source = ./config.jsonc;
+    xdg.configFile."waybar/style.css".source = ./style.css;
+  };
+
+}
