d8ee0cebed99db3bb5d4f85a13242431315f8303
Autumn McKee <autumn@mck.is>
2024-07-12 19:01:04 +0100

    add hawthorn hardware config

---
+++ hardware/hawthorn/hardware.nix
@@ -0,0 +1,16 @@
+{ config, nixos-hardware, pkgs, lib, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
+  console.keyMap = "uk";
+}
