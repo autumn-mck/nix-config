3bfa93a3e9fc62f213005d8e1320e0495de7500e
Autumn McKee <autumn@mck.is>
2024-09-12 14:14:44 +0100

    adding cosmic

--- flake.nix
+++ flake.nix
@@ -1,17 +1,32 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     catppuccin.url = "github:catppuccin/nix";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, catppuccin, ... }@attrs: {
+  outputs = { self, nixpkgs, nixos-hardware, catppuccin, nixos-cosmic, ... }@attrs: {
     nixosConfigurations.cherry = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
       modules = [
+        {
+          nix.settings = {
+            substituters = [ "https://cosmic.cachix.org/" ];
+            trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+          };
+        }
+        nixos-cosmic.nixosModules.default
         catppuccin.nixosModules.catppuccin
         ./configuration.nix
         ./hardware/cherry/hardware.nix
