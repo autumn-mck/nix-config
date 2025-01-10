55ddd1d8c32defdc6b670e8a8c2e7195b3a2ba56
Autumn McKee <autumn@mck.is>
2025-01-10 01:48:31 +0000

    bodge a bunch of systemd units to get stuff on startup

--- server/willow.nix
+++ server/willow.nix
@@ -5,6 +5,43 @@
     environment.systemPackages = with pkgs; [
     ];
 
+    systemd.services."music-display" = {
+      description = "music-display";
+      wantedBy = [ "default.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.fish}/bin/fish -c 'cd /home/autumn/MusicDisplayServer && bun dev'";
+        Restart = "always";
+        WorkingDirectory = "/home/autumn/MusicDisplayServer";
+        User = "autumn";
+      };
+    };
+
+    systemd.services."gnss-mqtt" = {
+      description = "gnss-mqtt";
+      wantedBy = [ "default.target" ];
+      wants = [ "podman.socket" ];
+      after = [ "podman.socket" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.fish}/bin/fish -c 'sleep 1 && cd /home/autumn/mqtt && podman compose up'";
+        Restart = "always";
+        WorkingDirectory = "/home/autumn/mqtt";
+        User = "autumn";
+      };
+    };
+
+    systemd.services."gnss-war-room" = {
+      description = "gnss-war-room";
+      wantedBy = [ "default.target" ];
+      wants = [ "gnss-mqtt.service" ];
+      after = [ "gnss-mqtt.service" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.fish}/bin/fish -c 'sleep 2 && nix-shell --run \"./webStart.sh\"'";
+        Restart = "always";
+        WorkingDirectory = "/home/autumn/gnss-war-room";
+        User = "autumn";
+      };
+    };
+
     caddy.enable = true;
     forgejo.enable = true;
     services.caddy = {
