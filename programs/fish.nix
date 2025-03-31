1cb9dcac26b639e6cda96dce834413b3e1691f96
Autumn McKee <autumn@mck.is>
2025-03-31 21:38:04 +0100

    fix fish pure plugin, fix grc, add bang-bang

--- programs/fish.nix
+++ programs/fish.nix
@@ -17,20 +17,30 @@
         set fish_greeting # Disable greeting
       '';
       shellAbbrs = {
-        update = "sudo nix flake update /home/autumn/Desktop/Projects/nix-config/ && rm ~/.gtkrc-2.0 && sudo nixos-rebuild switch --flake '/home/autumn/Desktop/Projects/nix-config/#'";
         what = "nvd diff $(ls -d1v /nix/var/nix/profiles/system-*-link|tail -n 2)";
       };
     };
 
-    environment.systemPackages = with pkgs; [
-      grc
-      fishPlugins.pure
-      fishPlugins.autopair
-      fishPlugins.sponge
-      fishPlugins.done
-      fishPlugins.z
-      fishPlugins.grc
-      fishPlugins.puffer
-    ];
+    environment.systemPackages =
+      with pkgs.fishPlugins;
+      [
+        grc
+        # fishPlugins.pure
+        (pure.overrideAttrs {
+          nativeCheckInputs = [ ];
+          checkPlugins = [ ];
+          checkPhase = "";
+        })
+        autopair
+        sponge
+        done
+        z
+        grc
+        puffer
+        bang-bang
+      ]
+      ++ (with pkgs; [
+        grc
+      ]);
   };
 }
