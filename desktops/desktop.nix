8cd6577a928540170385a97af2cc4a25d43ad2c8
Autumn McKee <autumn@mck.is>
2025-04-29 19:51:27 +0100

    fix dolphin not realising it can open files

--- desktops/desktop.nix
+++ desktops/desktop.nix
@@ -137,6 +137,9 @@
       })
     ];
 
+    environment.etc."/xdg/menus/applications.menu".text =
+      builtins.readFile "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+
     nixpkgs.overlays = [
       (final: prev: {
         waybar = prev.waybar.overrideAttrs (old: {
